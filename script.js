0521fa5702ad5f59b37a59c600ddacf0356834d5
Web-Code <ahmedwahaj317@gmail.com>
2025-05-17 09:45:43 +0500

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,12 @@ const quizData = [{
     d: "none of the above",
     correct: "b",
 },
+{
+    question: "What does CSS stands for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "b",
+}
+];
